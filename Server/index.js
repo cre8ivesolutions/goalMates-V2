dfeb2f079cd32f4bbe93f84d99b4d4f7f54817c2
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-10-23 19:40:04 -0500

    added my db.js

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 const cors = require ('cors')
 const pool = require("./db")
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 // const axios = require('axios')
 require("dotenv").config();
 
